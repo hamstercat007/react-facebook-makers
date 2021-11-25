6372043b13146152497771664d360bd445995f12
hamstercat007 <lisamac421@gmail.com>
2021-11-25 19:00:11 +0000

    Added cat component text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import saxophone from "./saxophone_cat_400.png";
 import "./App.css";
+import Cat from "./Cat";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={saxophone} className="App-logo" alt="saxophone_cat" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <br />
+        <Cat />
+        <a href="https://purrcity.org/">Join me here</a>
       </header>
     </div>
   );
---
+++ src/Cat.js
@@ -0,0 +1,9 @@
+
+
+function Cat() {
+  return (
+    <div><span>Cats are cute</span></div>
+  )
+}
+
+export default Cat
